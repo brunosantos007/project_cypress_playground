66609a45db4a792998dc0b3e65adf942d8678670
Bruno <bruno.santos@gmail.com>
2026-03-25 12:26:45 +1000

    cypress_project_05

---
+++ cypress/e2e/forms/date_picker/date_picker.cy.js
@@ -0,0 +1,60 @@
+/// <reference types="cypress" />
+
+beforeEach('Open the App', () => {
+    cy.visit('/')
+    cy.contains('Forms').click()
+    cy.contains('Datepicker').click()
+    cy.get('div > a.sidebar-toggle').click()
+})
+
+
+describe('Common Datepicker', () => {
+
+    it('Selecting Date', () => {
+        
+        let date = new Date()
+        // Example of how to find an exactly day that we could need
+        //date.setDate(date.getDate() + 3)
+        let todaysDate = date.getDate()
+        let dateToAssert = `Mar ${todaysDate}, 2026`
+
+        cy.get('[placeholder="Form Picker"]').then(input => {
+            cy.wrap(input).click()
+            cy.get('nb-calendar-day-cell').not('.bounding-month').contains(todaysDate).click()
+            cy.wrap(input).should('have.value', dateToAssert)
+        })
+    });
+
+    it.only('Selecting Date from other Months', () => {
+
+        function selectDateFromCurrentDay(day) {
+            let date = new Date()
+            date.setDate(date.getDate() + day)
+            let futureDay = date.getDate()
+            let futureMonthLong = date.toLocaleDateString('en-US', { month: 'long'})
+            let futureMonthShort = date.toLocaleDateString('en-US', { month: 'short'})
+            let futureYear = date.getFullYear()
+            let dateToAssert = `${futureMonthShort} ${futureDay}, ${futureYear}`
+
+            cy.get('nb-calendar-view-mode').invoke('text').then(calendarMonthAndYear => {
+
+                if (!calendarMonthAndYear.includes(futureMonthLong) || !calendarMonthAndYear.includes(futureYear)) {
+                    cy.get('[data-name="chevron-right"]').click()
+                    selectDateFromCurrentDay(day)
+                } else {
+                    cy.get('.day-cell').not('.bounding-month').contains(futureDay).click()
+                }
+
+            })
+            return dateToAssert
+        }
+
+        
+        cy.get('[placeholder="Form Picker"]').then(input => {
+            cy.wrap(input).click()
+            const dateToAssert = selectDateFromCurrentDay(20)
+            cy.wrap(input).should('have.value', dateToAssert)
+        })
+    });
+
+})
--- cypress/e2e/tables_and_data/smart_table.cy.js
+++ cypress/e2e/tables_and_data/smart_table.cy.js
@@ -34,7 +34,7 @@ describe('Web Tables', () => {
         })
     });
 
-    it.only('Create a new User', () => {
+    it('Create a new User', () => {
         cy.get('.nb-plus').click()
         cy.get('thead tr').eq(2).then(tableRow => {
             cy.wrap(tableRow).find('[placeholder="First Name"]').type('Ramirez')
@@ -45,6 +45,22 @@ describe('Web Tables', () => {
         cy.get('tbody tr').first().find('td').then( tableCol => {
             cy.wrap(tableCol).eq(2).should('have.text', 'Ramirez')
             cy.wrap(tableCol).eq(3).should('have.text', 'Diaz')
+        })
+    });
+
+    it.only('Filter Data from Web Table', () => {
+        const ages = [20, 30, 40, 200]
+
+        cy.wrap(ages).each(age => {
+        cy.get('[placeholder="Age"]').clear().type(age)
+        cy.wait(500)
+        cy.get('tbody tr').last().each(tableRow => {
+            if (age == 200) {
+                cy.get('td').should('not.have.text', age)
+            } else {
+                cy.wrap(tableRow).find('td').last().should('have.text', age)
+            }
+        })
         })
     });
 })
