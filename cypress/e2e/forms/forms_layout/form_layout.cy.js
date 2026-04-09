6c4d222a48a38fef154b07f2108f525b771f2817
Bruno <brunogsantoss@outlook.com>
2026-04-09 13:00:40 +1000

    cypress_project_15

--- cypress/e2e/forms/forms_layout/form_layout.cy.js
+++ cypress/e2e/forms/forms_layout/form_layout.cy.js
@@ -1,28 +1,54 @@
 /// <reference types="cypress" />
 
+import { describe } from "mocha";
 import { form_layout_page } from "../../../page_objects/form_layout/form_layout_page"
 import { navigateTo } from "../../../page_objects/navigationPage"
 import { faker } from '@faker-js/faker';
 
 
 describe('Inline Form', {tags: '@formOne'}, () => {
-    const nameTest = 'Testador'
-    it('Inline Form Completed', () => {
+
+    it('Inline Form: no validation', () => {
+        navigateTo.forms_layout_Page()
+    });
+    it('Inline Form: valid input (no submission)', () => {
         navigateTo.forms_layout_Page()
         form_layout_page.inlineForm()
     })
 })
 
 describe('Using the Grid', () => {
-    it('Grid', () => {
+
+    it('Using the Grid: no validation', () => {
+        navigateTo.forms_layout_Page()
+        form_layout_page.gridFormNoValidation()
+    });
+    it('Using the Grid: valid input (no submission)', () => {
         navigateTo.forms_layout_Page()
         form_layout_page.gridForm()
     })
 })
 
 describe('Basic Form', () => {
-    it.only('Basic Form Completed', () => {
+
+    it('Basic Form: no validation', () => {
+        navigateTo.forms_layout_Page()
+        form_layout_page.basicFormNoValidation()
+    });
+    it('Basic Form: valid input (no submission)', () => {
         navigateTo.forms_layout_Page()
         form_layout_page.basicForm()
+    });
+})
+
+describe('Form Without Labels', () => {
+    it('Form Without Labels: no validation', () => {
+        navigateTo.forms_layout_Page()
+        form_layout_page.formWithoutLabelsNoValidation()
+    });
+
+    it('Form Without Labels: valid input (no submission)', () => {
+        navigateTo.forms_layout_Page()
+        form_layout_page.formWithoutLabels()
     });
 })
--- cypress/page_objects/form_layout/form_layout_page.js
+++ cypress/page_objects/form_layout/form_layout_page.js
@@ -13,8 +13,13 @@ class formLayoutPage {
         cy.get('.form-inline > nb-checkbox.status-basic > .label > .custom-checkbox').click()
         cy.get('nb-card-body > form > input:nth-child(1)').invoke('prop', 'value').then(value => {
             console.log(value)
+        })   
+    }
+
+    inlineFormNoValidation() {
+        cy.contains('nb-card', 'Using the Grid').then(allElements => {
+            cy.wrap(allElements).find('[status="primary"]').click()
         })
-            
     }
 
     gridForm() {
@@ -26,12 +31,39 @@ class formLayoutPage {
         })
     }
 
+    gridFormNoValidation() {
+        cy.contains('nb-card', 'Using the Grid').then(allElements => {
+            cy.wrap(allElements).find('[status="primary"]').click()
+        })
+    }
+
     basicForm() {
         cy.contains('nb-card', 'Basic form').then(elementsBasicForm => {
             cy.wrap(elementsBasicForm).find('[id="exampleInputEmail1"]').type(`${faker.internet.email()}`)
             cy.wrap(elementsBasicForm).find('[id="exampleInputPassword1"]').type(`${faker.internet.password()}`)
             cy.wrap(elementsBasicForm).find('[type="checkbox"]').check({ force: true })
             cy.wrap(elementsBasicForm).find('[class="appearance-filled size-medium shape-rectangle status-danger nb-transition"]').click()
+        })
+    }
+
+    basicFormNoValidation() {
+        cy.contains('nb-card', 'Basic form').then(elementsBasicForm => {
+            cy.wrap(elementsBasicForm).find('[class="appearance-filled size-medium shape-rectangle status-danger nb-transition"]').click()
+        })
+    }
+
+    formWithoutLabels() {
+        cy.contains('nb-card', 'Form without labels').then(elementsformWithoutLabels => {
+            cy.wrap(elementsformWithoutLabels).find('[placeholder="Recipients"]').type(`${faker.food.ingredient}`)
+            cy.wrap(elementsformWithoutLabels).find('[placeholder="Subject"]').type(`${faker.food.description}`)
+            cy.wrap(elementsformWithoutLabels).find('[placeholder="Message"]').type(`${faker.food.ethnicCategory}`)
+            cy.wrap(elementsformWithoutLabels).find('nbbutton').click()
+        })
+    }
+
+    formWithoutLabelsNoValidation() {
+        cy.contains('nb-card', 'Form without labels').then(elementsformWithoutLabels => {
+            cy.wrap(elementsformWithoutLabels).find('nbbutton').click()
         })
     }
 }
