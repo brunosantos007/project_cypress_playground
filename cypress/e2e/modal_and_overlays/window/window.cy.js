d2f659629bf3c45954b8e92511bb4cce397525a5
Bruno <brunogsantoss@outlook.com>
2026-05-30 12:16:32 +1000

    cypress_project_23

--- cypress/e2e/modal_and_overlays/window/window.cy.js
+++ cypress/e2e/modal_and_overlays/window/window.cy.js
@@ -9,8 +9,33 @@ describe('Window Form', () => {
         window_page.openWindowForm()
     });
 
+    it('Minimize Window Form', () => {
+        navigateTo.window_Page()
+        window_page.minimizeWindowForm()
+    });
+
+    it('Collapse Window Form', () => {
+        navigateTo.window_Page()
+        window_page.collapseWindowsForm()
+    });
+
     it('Open Window Without Backdrop', () => {
         navigateTo.window_Page()
         window_page.openWindowWithoutBackdrop()
+    });
+
+    it('Minimize Window Without Backdrop', () => {
+        navigateTo.window_Page()
+        window_page.minimizeWindowWithoutBackdrop()
+    });
+
+    it('Collapse Window Without Backdrop', () => {
+        navigateTo.window_Page()
+        window_page.collapseWindowWithoutBackdrop()
+    });
+
+    it.only('Open Homepage in a New Tab', () => {
+        navigateTo.window_Page()
+        window_page.openHomePageInANewTab()
     });
 })
--- cypress/page_objects/modals_&_overlays/window/window.js
+++ cypress/page_objects/modals_&_overlays/window/window.js
@@ -10,12 +10,52 @@ class windowPage {
         })
     }
 
+    minimizeWindowForm() {
+        cy.contains('nb-card', 'Window Form').then(elementsOpenWindowForm => {
+            cy.wrap(elementsOpenWindowForm).contains('button', 'Open window form').click()
+            cy.get('nb-icon[icon="minus-outline"]').click()
+            cy.get('.minimized').should('exist')
+        })
+    }
+
+    collapseWindowsForm() {
+        cy.contains('nb-card', 'Window Form').then(elementsOpenWindowForm => {
+            cy.wrap(elementsOpenWindowForm).contains('button', 'Open window form').click()
+            cy.get('nb-icon[icon="collapse-outline"').click()
+            cy.get('.maximized').should('exist')
+        })
+    }
+
     openWindowWithoutBackdrop() {
         cy.contains('nb-card', 'Window Form').then(elementsOpenWindowForm => {
             cy.wrap(elementsOpenWindowForm).contains('button', 'Open window without backdrop').click()
+            cy.get('.cdk-overlay-container').click({ force: true })
             cy.get('nb-window.full-screen').should('exist')
         })
     }
+
+    minimizeWindowWithoutBackdrop() {
+        cy.contains('nb-card', 'Window Form').then(elementsOpenWindowForm => {
+            cy.wrap(elementsOpenWindowForm).contains('button', 'Open window without backdrop').click()
+            cy.get('nb-icon[icon="minus-outline"]').click()
+            cy.get('.minimized').should('exist')
+        })
+    }
+
+    collapseWindowWithoutBackdrop() {
+        cy.contains('nb-card', 'Window Form').then(elementsOpenWindowForm => {
+            cy.wrap(elementsOpenWindowForm).contains('button', 'Open window without backdrop').click()
+            cy.get('nb-icon[icon="collapse-outline"').click()
+            cy.get('.maximized').should('exist')
+        })
+    }
+
+    openHomePageInANewTab() {
+        cy.contains('nb-card', 'New Tab').then(elementsOpenWindowForm => {
+            cy.wrap(elementsOpenWindowForm).contains('button', 'Open homepage in a new tab').click()
+        })
+    }
+
 }
 
 export const window_page = new windowPage()
